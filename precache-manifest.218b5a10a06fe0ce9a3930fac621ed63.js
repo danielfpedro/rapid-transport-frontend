40624506928f5e5faf415932c9c5cbe4092a061b
danielfpedro <danielfpedro@gmail.com>
2020-10-21 12:00:49 -0300

    Updates

---
+++ precache-manifest.218b5a10a06fe0ce9a3930fac621ed63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea05d6ecb819673db844c50f1203a9de",
+    "url": "/rapid-transport-frontend/index.html"
+  },
+  {
+    "revision": "eaa821f78180717c0d15",
+    "url": "/rapid-transport-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a20926bfda2e5604334",
+    "url": "/rapid-transport-frontend/static/js/2.5f6a93be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/rapid-transport-frontend/static/js/2.5f6a93be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa821f78180717c0d15",
+    "url": "/rapid-transport-frontend/static/js/main.0c66b1ef.chunk.js"
+  },
+  {
+    "revision": "4424928a0d87ee1ca525",
+    "url": "/rapid-transport-frontend/static/js/runtime-main.c0e47fbf.js"
+  }
+]);
